d1e30ceab943c3bc3ccc2b03fd90c6e73c7e900e
o.koch <o.koch@gputest025-pa4.central.criteo.preprod>
2019-04-16 12:24:29 +0000

    with validation

--- ML20m/PW2V_hard.py
+++ ML20m/PW2V_hard.py
@@ -1,5 +1,6 @@
 from KNearestNeighborsRec import KNearestNeighborsRecModel
 from data_processing import load_data, load_tags, filter_movies_with_no_tags, build_index_dictionary, get_movie_vocab, compute_metrics, build_product_embeddings, compute_cold_start_metrics
+import datetime
 import random
 import math
 import os
@@ -18,7 +19,7 @@ embedding_size = 200
 window_size = 3
 neg_samples = 20
 learn_rate = 1e-4
-num_steps = 200001
+num_steps = 2001
 optimizer="adam"
 alternate_losses_step = __alternate_loss_steps
 
@@ -32,6 +33,9 @@ print("done")
 
 train, test = filter_movies_with_no_tags(train, test, movie_tags)
 movies_train = list(train.values())
+train_validation_cut = int(0.8*len(movies_train))
+movies_train_train = movies_train[:train_validation_cut]
+movies_train_validation = movies_train[train_validation_cut:]
 movie_tags_list = list(movie_tags.values())
 vocab_size = len(vocab)
 # avg tags per movie = 17.78249347853091
@@ -46,7 +50,7 @@ del train_sets
 # del vocab
 print("building graph...")
 user_index = 0  # where to start generating batch from
-tag_index = 0
+user_index_validation = 0
 
 class ContentEmbToRec(KNearestNeighborsRecModel):
 
@@ -75,7 +79,7 @@ def eval (word_embeddings):
     return (hr, ndcg, cs_hr, cs_ndcg)
 
 
-def generate_batch(batch_size, window_size):
+def generate_batch_train(batch_size, window_size):
     # window_size = window_size for products
     global user_index
     # src words (batch) and context words (labels)
@@ -86,29 +90,64 @@ def generate_batch(batch_size, window_size):
     labels_movies = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
     batch_index = 0
     while True:
-        num_user_movies = len(movies_train[user_index])
+        num_user_movies = len(movies_train_train[user_index])
         for i in range(num_user_movies):
-            for tag in movie_tags[movies_train[user_index][i]]:
+            for tag in movie_tags[movies_train_train[user_index][i]]:
                 window_start = max(i - window_size, 0)
                 window_end = min(num_user_movies, i + window_size)
                 for j in range(window_start, window_end):
                     if i != j:
                         batch_words[batch_index] = tag_dictionary[tag]
                         labels_words[batch_index, 0] = tag_dictionary[
-                            random.choice(movie_tags[movies_train[user_index][j]])]
+                            random.choice(movie_tags[movies_train_train[user_index][j]])]
 
-                        batch_movies[batch_index] = movie_dictionary[movies_train[user_index][i]]
-                        labels_movies[batch_index, 0] = movie_dictionary[movies_train[user_index][j]]
+                        batch_movies[batch_index] = movie_dictionary[movies_train_train[user_index][i]]
+                        labels_movies[batch_index, 0] = movie_dictionary[movies_train_train[user_index][j]]
 
                         batch_index += 1
                         if batch_index == batch_size:
                             user_index += 1
-                            if user_index == len(movies_train):
+                            if user_index == len(movies_train_train):
                                 user_index = 0
                             return batch_words, labels_words, batch_movies, labels_movies
         user_index += 1
-        if user_index == len(movies_train):
+        if user_index == len(movies_train_train):
             user_index = 0
+
+def generate_batch_validation(batch_size, window_size):
+    # window_size = window_size for products
+    global user_index_validation
+    # src words (batch) and context words (labels)
+    batch_words_validation = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels_words_validation = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+
+    batch_movies_validation = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels_movies_validation = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    batch_index = 0
+    while True:
+        num_user_movies = len(movies_train_validation[user_index_validation])
+        for i in range(num_user_movies):
+            for tag in movie_tags[movies_train_validation[user_index_validation][i]]:
+                window_start = max(i - window_size, 0)
+                window_end = min(num_user_movies, i + window_size)
+                for j in range(window_start, window_end):
+                    if i != j:
+                        batch_words_validation[batch_index] = tag_dictionary[tag]
+                        labels_words_validation[batch_index, 0] = tag_dictionary[
+                            random.choice(movie_tags[movies_train_validation[user_index_validation][j]])]
+
+                        batch_movies_validation[batch_index] = movie_dictionary[movies_train_validation[user_index_validation][i]]
+                        labels_movies_validation[batch_index, 0] = movie_dictionary[movies_train_validation[user_index_validation][j]]
+
+                        batch_index += 1
+                        if batch_index == batch_size:
+                            user_index_validation += 1
+                            if user_index_validation == len(movies_train_validation):
+                                user_index_validation = 0
+                            return batch_words_validation, labels_words_validation, batch_movies_validation, labels_movies_validation
+        user_index_validation += 1
+        if user_index_validation == len(movies_train_validation):
+            user_index_validation = 0
 
 
 # main params
@@ -214,7 +253,7 @@ with tf.Session(graph=graph) as session:
 
     report_every_steps = 100
     for step in range(num_steps):
-        batch_words, labels_words, batch_movies, labels_movies = generate_batch(batch_size, window_size)
+        batch_words, labels_words, batch_movies, labels_movies = generate_batch_train(batch_size, window_size)
         feed_dict = {train_word_inputs: batch_words,
                      train_word_labels: labels_words,
                      train_movie_inputs: batch_movies,
@@ -232,13 +271,21 @@ with tf.Session(graph=graph) as session:
         average_loss_movie += movie_loss_val
 
         writer.add_summary(summary, step)
+
         if step % report_every_steps == 0:
+            # validation loss
+            batch_words_val, labels_words_val, batch_movies_val, labels_movies_val = generate_batch_validation(batch_size, window_size)
+            feed_dict = {train_word_inputs: batch_words_val,
+                         train_word_labels: labels_words_val,
+                         train_movie_inputs: batch_movies_val,
+                         train_movie_labels: labels_movies_val}
+            tag_loss_validation, movie_loss_validation = session.run([tag_loss, movie_loss], feed_dict=feed_dict)
+
             if step > 0:
                 average_loss_tag /= report_every_steps
                 average_loss_movie /= report_every_steps
             # The average loss is an estimate of the loss over the last 2000 batches.
-            print('Average loss TAG at step ', step, ': ', average_loss_tag)
-            print('Average loss MOVIE at step ', step, ': ', average_loss_movie)
+            print(f"Step = {step:>10}   Loss (tag,movie):    train = {average_loss_tag:>8.2f},{average_loss_movie:>8.2f}    validation = {tag_loss_validation:>8.2f}, {movie_loss_validation:>8.2f}")
             average_loss_tag = 0
             average_loss_movie = 0
 
@@ -257,5 +304,6 @@ with tf.Session(graph=graph) as session:
     writer.close()
 
     (hr, ndcg, hr_cs, ndcg_cs) = eval(final_embeddings)
-    print(f"Eval: precision = {hr:.2f} %, CS precision = {hr_cs:.2f} %")
+    print(hr, ndcg, hr_cs, ndcg_cs)
+    print(f"Eval: precision = {hr:>10.8f} %, CS precision = {hr_cs:>10.8f} %")
 
